6e72876ebd7845b0a2fd0d55df140e38a952ab7c
Muhammad Iqbal Rifai <iqbalrifai@noreply.example.org>
2018-07-04 19:13:24 +0200

    Upload files to 'client/src/helper'

---
+++ client/src/helper/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const config = {
+  // Initialize Firebase
+
+  apiKey: "AIzaSyBt1o_ptz8WW6eF-Sa2ybTjdm_BdO0D9WI",
+  authDomain: "anime-999e6.firebaseapp.com",
+  databaseURL: "https://anime-999e6.firebaseio.com",
+  projectId: "anime-999e6",
+  storageBucket: "anime-999e6.appspot.com",
+  messagingSenderId: "389822907706"
+};
